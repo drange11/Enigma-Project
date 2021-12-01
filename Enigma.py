906f629a06abbf2979c38ee7605b5c0eb89f5a58
Darian Range <darianvaughnrange@gmail.com>
2021-11-30 19:43:21 -0800

    initial commit

---
+++ Enigma.py
@@ -0,0 +1,7 @@
+class enigma:
+    def __init__():
+        print("ready to encrypt file")
+    def encrypt():
+        print("File now encrypted")
+    def decrypt():
+        print("File now decrypted")
