26b4b32d6aef7ca1e2bd5bf584b5c42cdcc09826
Darian Range <darianvaughnrange@gmail.com>
2021-12-29 11:23:19 -0800

    tested the settings function to ensure that is working just needs the plug board and the gui most of the algorithms are done

--- Enigma.py
+++ Enigma.py
@@ -1,6 +1,10 @@
 import TopGroup
 
 myTopGroup = TopGroup.TopGroup()
+setting1 = input("please input your setting for rotor 1 ")
+setting2 = input("please input your setting for rotor 1 ")
+setting3 = input("please input your setting for rotor 1 ")
+myTopGroup.setRotorSetting(int(setting1), int(setting2), int(setting3))
 myChar = input("input your message that you would like to encript ")
 myChar = myTopGroup.encriptMessage(myChar)
 print(myChar)
